816c7fe2ca27fbbc60a1a2b13dc92bf6ff54a06e
Office52 <163460447+Office52@users.noreply.github.com>
2024-11-29 03:55:40 -0500

    Update Notification.java
    Added documentation

--- Notification.java
+++ Notification.java
@@ -4,6 +4,7 @@ public class Notification {
     private String message;
     private int sentTime;
 
+    //Initializes a notification object with the attributes: notificationID, recipient, message, sentTime
     public Notification(int notificationID, User recipient, String message, int sentTime) {
         this.notificationID = notificationID;
         this.recipient = recipient;
@@ -11,10 +12,12 @@ public class Notification {
         this.sentTime = sentTime;
     }
 
+    //Returns the recipient of the notification
     public User getRecipient() {
         return recipient;
     }
 
+    //The receieveNotification method is called to send the notification to the user
     public void send() {
         recipient.receiveNotification(this);
         System.out.println("Sending notification to " + recipient.getUserName() + ": " + message);
