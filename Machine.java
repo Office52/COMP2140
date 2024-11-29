1e442528ac73886e9f05c304c72a94673e840d77
Office52 <163460447+Office52@users.noreply.github.com>
2024-11-29 03:37:41 -0500

    Update Machine.java
    Added documentation

--- Machine.java
+++ Machine.java
@@ -6,7 +6,7 @@ public class Machine implements Serializable{
     private String type;
     private boolean status;
 
-    public Machine(int machineID, String type, boolean status){
+    public Machine(int machineID, String type, boolean status){ //Initialies the Machine object wuith the attributes, machineID, type and status
         this.machineID = machineID;
         this.type = type;
         this.status = status;
@@ -20,21 +20,21 @@ public class Machine implements Serializable{
         return type;
     }
 
-    public boolean isAvailable() {
+    public boolean isAvailable() { //Returns the status of the machine available or unavable
         return status;
     }
 
 
-    public void setStatus(boolean status) {
+    public void setStatus(boolean status) { //updates the machine status
         this.status = status;
     }
 
-    public void markUnavailable() {
+    public void markUnavailable() { //Changes status to false marking the machine as unavaliable
         this.status = false;
         System.out.println("Machine " + machineID + " is now unavailable.");
     }
 
-    public void markAvailable() {
+    public void markAvailable() { //Changes status to true, marking the machine as availabe
         this.status = true;
         System.out.println("Machine " + machineID + " is now available.");
     }
